d11ba9bc8a31f99922e0b63a61a4fb813566bfda
Rong Tao Luo <rongtao11@gmail.com>
2020-08-01 17:53:18 -0400

    a

--- app/src/main/java/com/example/unieats/ui/registration/FinalProfileFragment.kt
+++ app/src/main/java/com/example/unieats/ui/registration/FinalProfileFragment.kt
@@ -3,6 +3,7 @@ package com.example.unieats.ui.registration
 import android.content.Context
 import com.example.unieats.R
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,7 +12,10 @@ import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.example.unieats.ContextExtensions.hideKeyboard
+import com.example.unieats.MainActivity
 import com.example.unieats.databinding.FragmentFinalprofileBinding
+import com.example.unieats.models.History
+import com.example.unieats.models.User
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -44,11 +48,33 @@ class FinalProfileFragment : Fragment() {
         }
 
         binding.toProfile.setOnClickListener{
+            //database ref
+            val ref = FirebaseDatabase.getInstance().getReference("Users")
+
+            var noMatch = true
             ref.addValueEventListener(object : ValueEventListener {
                 override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    for  (childSnapshot in dataSnapshot.children){
 
+                        if(childSnapshot.child("username").getValue(String::class.java) == MainActivity.username){
+                            noMatch = false
+                            Log.e("FAIL", "user match")
+                            break;
+                        }
+
+                    }
+                    if(noMatch){
+                        val pusher = ref.push()
+                        val id = pusher.key
+
+                        val histor: Map<String, History> =mutableMapOf<String, History>()
+                        val userPush = User(id!!,MainActivity.firstName, MainActivity.lastName, histor ,MainActivity.username, MainActivity.password, "0", MainActivity.email, MainActivity.uni)
+                        //val userPush = User("","","",histor,"","","","","")
+                        pusher.setValue(userPush)
+                    }
+
+                    ref.removeEventListener(this);
                 }
-
 
                 override fun onCancelled(error: DatabaseError) {
                     // Failed to read value
